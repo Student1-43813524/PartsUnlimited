3b3d3dcc3fd1ca2b3c2c09f20bad5308aff269de
Student1-43813524 <Student1-43813524@LODSPRODMCA.onmicrosoft.com>
2024-09-11 07:46:25 -0700

    My commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
-    //Another change
+    //My first change
     public class CartItem : ILineItem
     {
         [Key]
