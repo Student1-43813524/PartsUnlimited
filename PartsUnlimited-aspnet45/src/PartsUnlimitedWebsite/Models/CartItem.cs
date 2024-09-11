85ff5f6a417800a6335c52b8fbb0823ba9a9f575
Student1-43813524 <Student1-43813524@LODSPRODMCA.onmicrosoft.com>
2024-09-11 06:19:36 -0700

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
-    //Adding a comment as a change
+    //Another change
     public class CartItem : ILineItem
     {
         [Key]
